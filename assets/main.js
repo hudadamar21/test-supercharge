73bea5b5c9ccabf7b74a7cf7b405cd8422afb99c
Huda Damar <hudadamar21@gmail.com>
2021-12-21 09:48:49 +0700

    add detail page: Create: One Page Business Website

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,8 @@
-import Typed from 'typed.js';
 
 AOS.init({
   duration: 500,
   once: true,
 });
-
-new Typed('#typed', {
-  strings: ['business', 'interactive'],
-  typeSpeed: 80,
-  backSpeed: 50,
-  loop: true,
-  backDelay: 2000
-});
-
 
 const navbar = document.querySelector('.navbar');
 const icon = document.querySelector('#si_icon');
@@ -27,7 +17,7 @@ document.addEventListener('scroll', () => {
   // jika scroll y diatas 500, ubah background menjadi putih dan text menjadi primary purple
   if(window.scrollY > containerHeight) {
     navbar.classList.add('bg-white/80', 'backdrop-blur')
-    navbar.classList.remove('bg-black/50')
+    navbar.classList.remove('bg-transparent')
     icon.classList.add('opacity-100')
     icon.classList.remove('opacity-0')
     menu.classList.remove('text-white')
@@ -35,7 +25,7 @@ document.addEventListener('scroll', () => {
     
     // jika scroll y diatas 0, ubah background menjadi hitam dan text putih
   } else if (window.scrollY > 0){
-    navbar.classList.add('bg-black/50', 'backdrop-blur')
+    navbar.classList.add('bg-transparent', 'backdrop-blur')
     navbar.classList.remove('bg-white/80')
     icon.classList.add('opacity-0')
     icon.classList.remove('opacity-100')
@@ -83,8 +73,35 @@ menu.addEventListener('click', (e) => {
   }
 })
 
-// Click Outside Menu to Close Menu
+// Share this label 
+const sharethisContainer = document.querySelector('.sharethis-container');
+const sharethisLabel = document.querySelector('.sharethis-label');
+
+const shareIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--icomoon-free w-8 h-8 fill-primary-purple"  preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path d="M13.5 11c-.706 0-1.342.293-1.797.763L4.969 8.396a2.46 2.46 0 0 0 0-.792l6.734-3.367a2.5 2.5 0 1 0-.672-1.341L4.297 6.263a2.5 2.5 0 1 0 0 3.474l6.734 3.367A2.5 2.5 0 1 0 13.5 11z"></path></svg>
+`
+
+sharethisLabel.addEventListener('click', (e) => {
+  e.stopPropagation()
+  sharethisContainer.classList.toggle('open')
+  if(sharethisContainer.classList.contains('open')) {
+    sharethisLabel.textContent = `Share this to :`
+  } else {
+    sharethisLabel.innerHTML = shareIcon
+  }
+})
+
+
+// Handle Clide Outside
 document.body.addEventListener('click', (e) => {
+
+  // Click Outside Menu to Sharethis 
+  if(sharethisContainer.classList.contains('open') && e.target !== sharethisContainer) {
+    sharethisContainer.classList.remove('open')
+    sharethisLabel.innerHTML = shareIcon
+  }
+
+  // Click Outside Menu to Close Menu
   if(navItem.classList.contains('open') && e.target !== navItem) {
     navItem.classList.remove('open')
     menu.innerHTML = menuIcon
@@ -92,4 +109,5 @@ document.body.addEventListener('click', (e) => {
       menu.classList.add('text-white')
     }
   }
+
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
+    "./*/**.html",
     "./assets/main.js"
   ],
   theme: {
